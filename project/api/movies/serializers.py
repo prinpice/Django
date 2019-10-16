a5829e60873c48df50e543f2b5aa7c7b510604ba
정찬미 <prinpice@naver.com>
2019-10-16 15:14:36 +0900

    [Update] Add command into serializers.py

--- project/api/movies/serializers.py
+++ project/api/movies/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Genre, Movie, Score
 
+# serializer : query set, model instance 등의 복잡한 데이터를 JSON, XML 등의 content type으로 쉽게 변환 가능한 python datatype으로 변환시켜줌
+
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
