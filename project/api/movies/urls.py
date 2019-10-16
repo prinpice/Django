1c70f89c134d814b2e430e066f2d91a6e5c2aa57
정찬미 <prinpice@naver.com>
2019-10-16 17:34:51 +0900

    [ãAdd] Add REST API code into urls.py & views.py

--- project/api/movies/urls.py
+++ project/api/movies/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('genres/', views.genre_list),
+    path('genres/<int:genre_pk>/', views.genre_detail),
+    path('movies/', views.movie_list),
+    path('movies/<int:movie_pk>/', views.movie_detail),
+    path('movies/<int:movie_pk>/scores/', views.score_create),
 ]
--- project/api/movies/views.py
+++ project/api/movies/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import GenreSerializer, MovieSerializer, ScoreSerializer
+from .models import Genre, Movie
 
 # Create your views here.
+
+@api_view(['GET'])
+def genre_list(request):
+    genres = Genre.objects.all()
+    serializer = GenreSerializer(genres, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def genre_detail(request, genre_pk):
+    genre_movies = Movie.objects.filter(genre=genre_pk)
+    serializer = MovieSerializer(genre_movies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def movie_list(request):
+    movie = Movie.objects.all()
+    serializer = MovieSerializer(movie, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def score_create(request, movie_pk):
+    if request.method == "POST":
+        print(request.data)
+        # movie_id는 serializer에 object형태로 넣거나, save()에서 id=1 등으로 직접 넣어주면 된다.
+        # commit=false 는 안됨
+        serializer = ScoreSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({"messages": "작성되었습니다."})
